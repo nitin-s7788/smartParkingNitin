23c32dd9565a8c9fb63a47c8c7b78ef3cf8e5ab7
nitin solanki <nsolanki7788@gmail.com>
2022-03-22 02:08:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ function App() {
 
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6218.451959679004!2d77.03202660886484!3d28.611461657195193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zMjjCsDM2JzM3LjIiTiA3N8KwMDInMTAuMCJF!5e0!3m2!1sen!2sin!4v1647890536570!5m2!1sen!2sin" width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy"></iframe>
 
+      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m20!1m8!1m3!1d2014.5233474392903!2d77.03504540492392!3d28.611591054817886!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d28.585850999999998!2d77.08063899999999!4m3!3m2!1d28.6103126!2d77.0359957!5e0!3m2!1sen!2sin!4v1647894272339!5m2!1sen!2sin" width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy"></iframe> */}
+
     </div>
   );
 }
